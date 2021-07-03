60c4c1b439a799f6ab567636dc61fff5122920e1
Shubhangvanjani <shubhangvanjani@gmail.com>
2021-07-03 19:56:10 +0530

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -28,9 +28,9 @@
           <li class="nav-item">
             <a class="nav-link js-scroll-trigger" href="#skills">Skills</a>
           </li>
-          <li class="nav-item">
+          <!-- <li class="nav-item">
             <a class="nav-link js-scroll-trigger" href="#interests">Interests</a>
-          </li>
+          </li> -->
           <li class="nav-item">
             <a class="nav-link js-scroll-trigger" href="#awards">Projects</a>
           </li>
@@ -75,22 +75,6 @@ opportunities.</p>
 
         <div class="">
            <div class="resume-date text-md-right">
-            <span class="text-primary">July 2019 - Present</span>
-          </div>
-          <div class="resume-content">
-            <h3 class="mb-0">Software Engineer</h3>
-            <div class="subheading mb-3">Office Beacon LLC</div>
-           
-            <p>
-              Developing web and mobile application using the Vuejs and Nativescript-vue integrated with salesforce services.</p>
-             <p> Developed a service in nodejs using the micro-service architecture to integrate the third party API (Donbest API) with the application .
-            </p>
-          </div>
-          
-        </div>
-
-        <div class="">
-           <div class="resume-date text-md-right">
             <span class="text-primary">January 2019 - June 2019</span>
           </div>
           <div class="resume-content">
@@ -104,6 +88,41 @@ opportunities.</p>
           </div>
           
         </div>
+
+        <div class="">
+           <div class="resume-date text-md-right">
+            <span class="text-primary">July 2019 - September 2020</span>
+          </div>
+          <div class="resume-content">
+            <h3 class="mb-0">Software Developer</h3>
+            <div class="subheading mb-3">Office Beacon LLC</div>
+           
+            <p>
+              Developed a web application along with the native application which can run on both platforms i.e Android and IOS using the nativescript-vue technology.</p>
+             <p> Developed a service in nodejs using the micro-service architecture to integrate the third party API (Donbest API) with the application .
+            </p>
+            <p>Developed the clone of the mobile application using React Native and released for both the platforms (Android and IOS).</p>
+            <p>Created a service in feathers.js to integrate third party api of stock libraries like Adobe, Flickr, Pixabay, Unsplash and Pexels.</p>
+          </div>
+          
+        </div>
+
+        <div class="">
+           <div class="resume-date text-md-right">
+            <span class="text-primary">March 2021 - Present</span>
+          </div>
+          <div class="resume-content">
+            <h3 class="mb-0">Software Developer </h3>
+            <div class="subheading mb-3">Archesoftronix India pvt. ltd.</div>
+             <p>Developed a middleware for third-party apis of team-work crm in node js.</p>
+              <p>Developing Accounting Software using React as the front-end.</p>
+              <p>Developed an invoice calculator app using React Native for front end and nodejs for backend and deployed for ios on App Store.
+            </p>
+          </div>
+          
+        </div>
+
+        
 
         <!-- <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
           <div class="resume-content">
@@ -226,13 +245,13 @@ opportunities.</p>
 
     <hr class="m-0">
 
-    <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="interests">
+    <!-- <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="interests">
       <div class="w-100">
         <h2 class="mb-5">Interests</h2>
-        <!-- <p>Apart from being a web developer, I enjoy most of my time being outdoors. In the winter, I am an avid skier and novice ice climber. During the warmer months here in Colorado, I enjoy mountain biking, free climbing, and kayaking.</p> -->
-        <!-- <p class="mb-0">When forced indoors, I follow a number of sci-fi and fantasy genre movies and television shows, I am an aspiring chef, and I spend a large amount of my free time exploring the latest technology advancements in the front-end web development world.</p> -->
+        <p>Apart from being a web developer, I enjoy most of my time being outdoors. In the winter, I am an avid skier and novice ice climber. During the warmer months here in Colorado, I enjoy mountain biking, free climbing, and kayaking.</p>
+        <p class="mb-0">When forced indoors, I follow a number of sci-fi and fantasy genre movies and television shows, I am an aspiring chef, and I spend a large amount of my free time exploring the latest technology advancements in the front-end web development world.</p>
       </div>
-    </section>
+    </section> -->
 
     <hr class="m-0">
 
@@ -240,34 +259,19 @@ opportunities.</p>
       <div class="w-100">
         <!-- <h2 class="mb-5">Awards &amp; Certifications</h2> -->
         <h2 class="mb-5">Projects</h2>
-        <!-- <ul class="fa-ul mb-0">
-          <li>
-            <i class="fa-li fa fa-trophy text-warning"></i>
-            Google Analytics Certified Developer</li>
-          <li>
-            <i class="fa-li fa fa-trophy text-warning"></i>
-            Mobile Web Specialist - Google Certification</li>
-          <li>
-            <i class="fa-li fa fa-trophy text-warning"></i>
-            1<sup>st</sup>
-            Place - University of Colorado Boulder - Emerging Tech Competition 2009</li>
-          <li>
-            <i class="fa-li fa fa-trophy text-warning"></i>
-            1<sup>st</sup>
-            Place - University of Colorado Boulder - Adobe Creative Jam 2008 (UI Design Category)</li>
-          <li>
-            <i class="fa-li fa fa-trophy text-warning"></i>
-            2<sup>nd</sup>
-            Place - University of Colorado Boulder - Emerging Tech Competition 2008</li>
-          <li>
-            <i class="fa-li fa fa-trophy text-warning"></i>
-            1<sup>st</sup>
-            Place - James Buchanan High School - Hackathon 2006</li>
-          <li>
-            <i class="fa-li fa fa-trophy text-warning"></i>
-            3<sup>rd</sup>
-            Place - James Buchanan High School - Hackathon 2005</li>
-        </ul> -->
+        <div class="">
+           <!-- <div class="resume-date text-md-right">
+            <span class="text-primary">January 2019 - June 2019</span>
+          </div> -->
+          <div class="resume-content">
+            <h3 class="mb-0">Inventory Management System</h3>
+            <!-- <div class="subheading mb-3">Office Beacon LLC</div> -->
+             <p>
+              Purpose to build the system is to manage the internal order placement in the manufacturing firm with large number of products.</p>
+              <p>Using Reactjs as front end.</p>
+          </div>
+          
+        </div>
       </div>
     </section>
 
@@ -277,7 +281,7 @@ opportunities.</p>
 
 <script>
 // @ is an alias to /src
-
+/* eslint-disable */
 export default {
   name: 'home',
   mounted() {
